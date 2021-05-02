04977796ee13b483f8de80fee5ce35ca63d63054
David Gee <cdhgee@users.noreply.github.com>
2021-05-02 17:41:27 +0000

    Fix unattended upgrades script

--- enable-unattended-upgrades.sh
+++ enable-unattended-upgrades.sh
@@ -11,6 +11,3 @@ apt install unattended-upgrades update-notifier-common -y
 # Copy config files
 cp ./config-files/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
 cp ./config-files/50unattended-upgrades /etc/apt.conf.d/50unattended-upgrades
-
-# Enable unattended upgrades
-dpkg-reconfigure --priority=low unattended-upgrades
